44ceb4a9802bd64ea80527a24faefcdc1854820d
Fchkn <fchkn.perdev@gmail.com>
2022-11-12 07:30:08 +0900

    プロフィール画像設定のプレビュー表示

--- src/templates/Users/edit.php
+++ src/templates/Users/edit.php
@@ -14,16 +14,22 @@
         </div>
     </div>
 
+    <!-- プロフィール情報 -->
     <div class="row border-bottom py-2">
         <div class="col-12 align-self-center"><span class="text-secondary">プロフィール情報</span></div>
     </div>
     <div class="row mb-5 py-4">
-        <div class="col-8 align-self-center text-left"><small class="text-muted">プロフィール画像</small></div>
-        <div class="col-4 text-right"><input type="submit" class="btn btn-secondary btn-sm" name="edit_profileinfo" value="変更を保存"/></div>
-        <div class="col-2 text-left"><img src="/upload/profile_img/user_<?php echo $auth_user->id ?>.jpg?<?php echo $img_param ?>" alt="profile_img" class="img-thumbnail mr-1" style="min-width:60px; min-height:60px;">
-    </div>
+        <div class="col-8 align-self-center text-left">
+            <small class="text-muted">プロフィール画像</small>
+        </div>
+        <div class="col-4 text-right">
+            <input type="submit" class="btn btn-secondary btn-sm" name="edit_profileinfo" value="変更を保存"/>
+        </div>
+        <div class="col-2 text-left">
+            <img src="/upload/profile_img/user_<?php echo $auth_user->id ?>.jpg?<?php echo $img_param ?>" alt="profile_img" class="img-thumbnail mr-1" style="min-width:60px; min-height:60px;" id="profile_img_view">
+        </div>
         <div class="col-10 m-0 align-self-center text-left form-group">
-            <input type="file" class="form-control-file" name="profile_img" value="ファイルを選択">
+            <input type="file" class="form-control-file" id="profile_img" name="profile_img" value="ファイルを選択">
             <p class="m-0 text-secondary">設定できる画像は、5MB以内・JPG/PNG形式です。</p>
         </div>
         <div class="col-8 mt-3 text-left">
@@ -33,36 +39,66 @@
         <div class="col-4"></div>
     </div>
 
+    <!-- メールアドレス -->
     <div class="row border-bottom py-2">
-        <div class="col-12 align-self-center"><span class="text-secondary">メールアドレス</span></div>
+        <div class="col-12 align-self-center">
+            <span class="text-secondary">メールアドレス</span>
+        </div>
     </div>
     <div class="row mb-5 py-4">
-        <div class="col-8 text-left"><input type="text" class="form-control" name="email" placeholder="メールアドレス" value="<?php echo $auth_user->email ?>"></div>
-        <div class="col-4 text-right"><input type="submit" class="btn btn-secondary btn-sm" name="edit_email" value="変更を保存"/></div>
+        <div class="col-8 text-left">
+            <input type="text" class="form-control" name="email" placeholder="メールアドレス" value="<?php echo $auth_user->email ?>">
+        </div>
+        <div class="col-4 text-right">
+            <input type="submit" class="btn btn-secondary btn-sm" name="edit_email" value="変更を保存"/>
+        </div>
     </div>
 
+    <!-- パスワード -->
     <div class="row border-bottom py-2">
-        <div class="col-12 align-self-center"><span class="text-secondary">パスワード</span></div>
+        <div class="col-12 align-self-center">
+            <span class="text-secondary">パスワード</span>
+        </div>
     </div>
     <div class="row mb-5 py-4">
-        <div class="col-8 text-left"><input type="text" class="form-control" name="password_now" placeholder="現在のパスワード"></div>
-        <div class="col-4 text-right"><input type="submit" class="btn btn-secondary btn-sm" name="edit_password" value="変更を保存"/></div>
-        <div class="col-8 mt-3 text-left"><input type="text" class="form-control" name="password_new" placeholder="新しいパスワード"></div>
+        <div class="col-8 text-left">
+            <input type="text" class="form-control" name="password_now" placeholder="現在のパスワード">
+        </div>
+        <div class="col-4 text-right">
+            <input type="submit" class="btn btn-secondary btn-sm" name="edit_password" value="変更を保存"/>
+        </div>
+        <div class="col-8 mt-3 text-left">
+            <input type="text" class="form-control" name="password_new" placeholder="新しいパスワード">
+        </div>
         <div class="col-4"></div>
-        <div class="col-8 mt-3 text-left"><input type="text" class="form-control" name="password_new_re" placeholder="新しいパスワード（再入力）"></div>
+        <div class="col-8 mt-3 text-left">
+            <input type="text" class="form-control" name="password_new_re" placeholder="新しいパスワード（再入力）">
+        </div>
         <div class="col-4"></div>
     </div>
 
+    <!-- Myliji退会 -->
     <div class="row border-bottom py-2">
         <div class="col-12 align-self-center"><span class="text-secondary">MyKiji退会</span></div>
     </div>
     <div class="row mb-5 py-4">
-        <div class="col-8 text-left"><span class="text-danger">※退会するとアカウントは復元できません。<br>　投稿した記事は全て削除されます。</span></div>
+        <div class="col-8 text-left"><span class="text-danger">※退会するとアカウントは復元できません。<br>&emsp;投稿した記事は全て削除されます。</span></div>
         <div class="col-4 text-right"><input type="button" class="btn btn-secondary btn-sm" onclick="location.href='/users/delete'" value="退会手続きに進む"/></div>
     </div>
 
+    <!-- 戻るボタン -->
     <div class="row py-5 justify-content-center">
         <input type="button" class="btn btn-secondary btn-lg" onclick="location.href='/users/view?user_id=<?php echo $auth_user->id ?>'" value="Myページに戻る"/>
     </div>
 </div>
 </form>
+
+<script>
+$("#profile_img").on("change", function (e) {
+    var reader = new FileReader();
+    reader.onload = function (e) {
+        $("#profile_img_view").attr("src", e.target.result);
+    }
+    reader.readAsDataURL(e.target.files[0]);
+});
+</script>
