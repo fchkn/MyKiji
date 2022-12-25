1fcd2c191fbb03f69d5dcedba840064824167170
Fchkn <fchkn.perdev@gmail.com>
2022-12-26 06:43:57 +0900

    投稿記事・お気に入り記事・フォロー・フォロワーリストのレスポンシブ対応

--- src/templates/element/follow_list.php
+++ src/templates/element/follow_list.php
@@ -1,20 +1,22 @@
 <div class="list-group px-5">
     <?php foreach ($follows as $follow) : ?>
         <!-- 記事リスト要素 -->
-        <div class="list-group-item list-group-item-action">
-            <div class="row align-items-center">
-                <div class="col-1 pl-2">
-                    <!-- プロフィール画像 -->
-                    <button type='button' class="icon-btn" onclick="location.href='/users/view?user_id=<?php echo $follow->{$target . '_user'}->id?>'">
-                        <img src="/upload/profile_img/user_<?php echo $follow->{$target . '_user'}->id ?>.jpg?<?php echo $img_param ?>" alt="profile_img" class="img-thumbnail mr-1 userimg-article">
-                    </button>
-                </div>
-                <div class="col-11">
-                    <!-- ユーザー名 -->
-                    <p class="m-0 text-secondary"><?php echo $follow->{$target . '_user'}->name ?></p>
+        <a href="/users/view?user_id=<?php echo $follow->{$target . '_user'}->id?>" style="text-decoration: none;">
+            <div class="list-group-item list-group-item-action">
+                <div class="row align-items-center">
+                    <div class="col-auto pr-0">
+                        <!-- プロフィール画像 -->
+                        <button type='button' class="icon-btn">
+                            <img src="/upload/profile_img/user_<?php echo $follow->{$target . '_user'}->id ?>.jpg?<?php echo $img_param ?>" alt="profile_img" class="img-thumbnail mr-1 userimg-article">
+                        </button>
+                    </div>
+                    <div class="col pl-0">
+                        <!-- ユーザー名 -->
+                        <p class="text-secondary mb-0"><?php echo $follow->{$target . '_user'}->name ?></p>
+                    </div>
                 </div>
             </div>
-        </div>
+        </a>
     <?php endforeach; ?>
     <?php if ($hasPaginator): ?>
         <!-- ページネーション要素 -->
