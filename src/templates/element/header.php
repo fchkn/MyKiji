1bcdf55a5bd60ca3ca904c2ec5d5fb8465f6625e
Fchkn <fchkn.perdev@gmail.com>
2022-12-27 07:13:31 +0900

    ヘッダーのレスポンシブ対応

--- src/templates/element/header.php
+++ src/templates/element/header.php
@@ -1,20 +1,16 @@
 <div class="container-fluid">
     <div class="row py-4">
-        <div class="col-1"></div>
-        <div class="col-2 align-self-center">
+        <!-- サイトロゴ -->
+        <div class="col-3 order-md-1 align-self-center text-center">
             <h1><a href="<?= $this->Url->build('/') ?>">MyKiji</a></h1>
         </div>
-        <div class="col-1"></div>
-        <div class="col-4 align-self-center text-center">
-            <input type="text" class="form-control" id="search_box" onkeypress="redirectSearch()" maxlength="70" placeholder="記事を検索">
-        </div>
-        <div class="col-1"></div>
         <?php if($hasAuth): ?>
-            <div class="col-3 align-self-center text-left">
+            <!-- ユーザーメニュー -->
+            <div class="col order-md-3 align-self-center text-md-center text-right">
                 <div class="dropdown">
                     <button class="btn btn-light dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                         <img src="/upload/profile_img/user_<?php echo $auth_user->id ?>.jpg?<?php echo $img_param ?>" alt="profile_img" class="img-thumbnail mr-1" style="width:40px; height:40px;">
-                        <span class="h6 text-secondary"><?php echo $auth_user->name ?></span>
+                        <span class="text-secondary username-header"><?php echo $auth_user->name ?></span>
                     </button>
                     <div class="dropdown-menu">
                         <a class="dropdown-item text-secondary" href="/users/view?user_id=<?php echo $auth_user->id ?>">Myページ</a>
@@ -23,14 +19,17 @@
                 </div>
             </div>
         <?php else: ?>
-            <div class="col-1 align-self-center text-right">
-                <input type="button" class="btn btn-secondary btn-sm" onclick="location.href='/users/login'" value="ログイン"/>
-            </div>
-            <div class="col-1 align-self-center text-left">
+            <div class="col order-md-3 align-self-center text-md-center text-right">
+                <!-- ログインボタン -->
+                <input type="button" class="btn btn-secondary btn-sm mr-2" onclick="location.href='/users/login'" value="ログイン"/>
+                <!-- 新規登録ボタン -->
                 <input type="button" class="btn btn-secondary btn-sm" onclick="location.href='/users/add'" value="新規登録"/>
             </div>
-            <div class="col-1"></div>
         <?php endif; ?>
+        <!-- 検索バー -->
+        <div class="col-md-6 order-md-2 mt-md-0 mt-3 align-self-center text-center">
+            <input type="text" class="form-control w-75 d-inline-block" id="search_box" onkeypress="redirectSearch()" maxlength="70" placeholder="記事を検索">
+        </div>
     </div>
 </div>
 
